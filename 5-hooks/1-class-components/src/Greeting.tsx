c0ad8abaf217ac2d313649aae088fc3f0da95af3
Lilia Manukyan <liliam@synopsys.com>
2022-07-01 10:54:35 +0400

    passing state from parent to child component

--- 5-hooks/1-class-components/src/Greeting.tsx
+++ 5-hooks/1-class-components/src/Greeting.tsx
@@ -14,13 +14,24 @@ class Greeting extends React.Component<GreetingProps> {
   constructor(props: GreetingProps) {
     super(props);
     this.state = {
-      message: `hello from ${props.name}`,
+      message: Greeting.getNewMessage(props.name),
     };
   }
 
   static getDerivedStateFromProps(props: GreetingProps, state: GreetingState) {
     console.log(props, state);
+
+    if (props.name /*&& props.name !== state.message*/) {
+      const newState = { ...state };
+      newState.message = Greeting.getNewMessage(props.name);
+      return newState;
+    }
+
     return state;
+  }
+
+  static getNewMessage(name: string = '') {
+    return `hello from ${name}`;
   }
 
   render() {
