3d8ffac31cd901d1569d5b3c756d942fbecaa14f
stardvst <lilia.manukyan@hotmail.com>
2022-07-02 18:34:16 +0400

    getting started with useCallback

--- 5-hooks/2-hook-components/src/App.tsx
+++ 5-hooks/2-hook-components/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useReducer } from 'react';
+import React, { useCallback, useReducer, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Greeting from './GreetingFunctional';
@@ -28,12 +28,37 @@ const initialState = {
 
 function App() {
   const [{ enteredName, message }, dispatch] = useReducer(reducer, initialState);
+  const [startCount, setStartCount] = useState(0);
+  const [count, setCount] = useState(0);
+
+  const setCountCallback = useCallback(() => {
+    const inc = count + 1 > startCount ? count + 1 : Number(count + 1) + startCount;
+    setCount(inc);
+  }, [count, startCount]);
+
+  const onWelcomeBtnClick = () => {
+    setCountCallback();
+  };
+
+  const onChangeStartCount = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setStartCount(Number(e.target.value));
+  };
+
+  console.log('rendering app');
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <Greeting enteredName={enteredName} message={message} greetingDispatcher={dispatch} />
+
+        <div style={{ marginTop: '10px' }}>
+          <label htmlFor="count">enter a number and we'll increment it</label>
+          <br />
+          <input type="text" name="count" value={startCount} onChange={onChangeStartCount} style={{ width: '4rem' }} />
+          <p>{count}</p>
+          <button onClick={onWelcomeBtnClick}>increment count</button>
+        </div>
       </header>
     </div>
   );
