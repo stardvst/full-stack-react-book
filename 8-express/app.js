db173b35064039515437e35b0bb7c38b6a648e2b
stardvst <lilia.manukyan@hotmail.com>
2022-07-12 08:31:44 +0400

    created simple web api

--- 8-express/app.js
+++ 8-express/app.js
@@ -14,9 +14,50 @@ router.post('/b', (req, res, next) => {
   res.send(`hello from the other side, b. message is ${req.body.message}`);
 });
 
-app.use((req, res, next) => {
-  throw new Error('a failure occurred');
+router.get('/api/v1/users', (req, res, next) => {
+  const users = [
+    {
+      id: 1,
+      username: 'John',
+    },
+    {
+      id: 2,
+      username: 'Jane',
+    },
+    {
+      id: 3,
+      username: 'Jack',
+    },
+  ];
+
+  console.log(req.query.userid);
+  const user = users.find(user => user.id == req.query.userid);
+  res.send(`user ${user?.username}`);
 });
+
+router.post('/api/v1/groups', (req, res, next) => {
+  const groups = [
+    {
+      id: 1,
+      groupname: 'Admins',
+    },
+    {
+      id: 2,
+      groupname: 'Users',
+    },
+    {
+      id: 3,
+      groupname: 'Employees',
+    },
+  ];
+
+  const group = groups.find(group => group.id == req.body.groupid);
+  res.send(`group ${group?.groupname}`);
+});
+
+// app.use((req, res, next) => {
+//   throw new Error('a failure occurred');
+// });
 
 app.use(router);
 
